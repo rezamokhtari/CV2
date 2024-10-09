1c02dfe695d0c40945b5ef6be0868cfb9396c38f
reza <mail.rezamokhtari@gmail.com>
2024-10-09 16:09:49 +0330

    part 4

--- test 1-2.py
+++ test 1-2.py
@@ -2,18 +2,16 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 24.0, (640,480))
 
 while(True):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    out.write(frame)
+    #frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.line(frame,(100,200),(300,400),(0,0,255),10)
+    cv2.rectangle(frame, (100,200),(200,300),(0,255,0),10)
 
-    cv2.imshow('fram',gray)
+    cv2.imshow('webcam',frame)
     if cv2.waitKey(1) & 0XFF == ord('q'):
         break
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
