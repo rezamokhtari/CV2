2ac4df7498ed96522fb232664f545f9665cd4126
reza <mail.rezamokhtari@gmail.com>
2024-10-16 21:26:00 +0330

    part 7

--- pert 6.py
+++ pert 6.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image.jpg',cv2.IMREAD_COLOR)
+img1 = cv2.imread('image.jpg',cv2.IMREAD_COLOR)
+img2 = cv2.imread('image2.jpg',cv2.IMREAD_COLOR)
 
-part1 = img[100:200,100:200]
-img[300:400,300:400] = part1
+#add = img1 + img2
+#add = cv2.add(img1,img2)
+add = cv2.addWeighted(img1,0.4,img2,0.6,0)
 
-cv2.imshow('image',img)
+cv2.imshow('image',add)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
